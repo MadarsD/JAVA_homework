3ec53e61f732cff9cf6ba2f055b14d78751ffc4e
MadarsD <madars.davids7@gmail.com>
2022-03-11 15:11:06 +0200

    Date and time exercises v1

---
+++ src/main/java/io/codelex/dateandtimepractice/DatePeriodTest.java
@@ -0,0 +1,21 @@
+package io.codelex.dateandtimepractice;
+
+import java.time.LocalDate;
+
+public class DatePeriodTest {
+    public static void main(String[] args) {
+        LocalDate firstStart = LocalDate.of(2022, 2, 10);
+        LocalDate firstEnd = LocalDate.of(2022, 3, 20);
+        DatePeriod firstPeriod = new DatePeriod(firstStart, firstEnd);
+
+        LocalDate secondStart = LocalDate.of(2022, 2, 9);
+        LocalDate secondEnd = LocalDate.of(2022, 3, 21);
+        DatePeriod secondPeriod = new DatePeriod(secondStart, secondEnd);
+
+        DatePeriod overlap = firstPeriod.intersection(secondPeriod);
+        System.out.println(overlap);
+
+
+
+    }
+}
---
+++ src/main/java/io/codelex/dateandtimepractice/Exercise_1.java
@@ -0,0 +1,36 @@
+package io.codelex.dateandtimepractice;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class Exercise_1 {
+
+    public static void main(String[] args) {
+
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter a starting date [yyyy-mm-dd]: ");
+        String d1 = scan.nextLine();
+        System.out.print("Enter a ending date [yyyy-mm-dd]: ");
+        String d2 = scan.nextLine();
+        LocalDate start = LocalDate.parse(d1, format);
+        LocalDate end = LocalDate.parse(d2, format);
+
+        long workingHours = 0;
+
+        for (LocalDate date = start; date.isBefore(end.plusDays(1)); date = date.plusDays(1)) {
+            if (date.getDayOfWeek() == DayOfWeek.SATURDAY || date.getDayOfWeek() == DayOfWeek.SUNDAY) {
+                workingHours += 0;
+            } else {
+                workingHours += 8;
+            }
+        }
+
+        System.out.println("In this period you have worked " + workingHours + " hours");
+    }
+}
+
+
