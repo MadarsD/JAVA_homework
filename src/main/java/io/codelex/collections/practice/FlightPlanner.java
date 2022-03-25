97759ddd5f3a84ebd5d3eda2fa41000a45ca8d3c
MadarsD <madars.davids7@gmail.com>
2022-03-25 09:39:28 +0200

    commit after further corrections

--- src/main/java/io/codelex/collections/practice/FlightPlanner.java
+++ src/main/java/io/codelex/collections/practice/FlightPlanner.java
@@ -11,30 +11,109 @@ import java.util.*;
 public class FlightPlanner {
     private static final Charset charset = Charset.defaultCharset();
     private static final String file = "/collections/flights.txt";
+    final Path path = Paths.get(Histogram.class.getResource(file).toURI());
+    List<String> list = new ArrayList<>(Files.readAllLines(path, charset));
 
+    Map<String, ArrayList<String>> flights = new HashMap<>();
+    static final ArrayList<String> cities = new ArrayList<>();
+
+
+    public FlightPlanner() throws IOException, URISyntaxException {
+    }
 
     public static void main(String[] args) throws IOException, URISyntaxException {
-        final Path path = Paths.get(Histogram.class.getResource(file).toURI());
-        List<String> list = new ArrayList<>(Files.readAllLines(path, charset));
-        Map<String, String> flights = new HashMap<>();
-        ArrayList<String> cities = new ArrayList<>();
-        ArrayList<String> destinations = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> route = new ArrayList<>();
+        FlightPlanner planner = new FlightPlanner();
 
+        planner.readFlightEntry();
+        System.out.println("what would you like to do:");
+        System.out.println("To display list of the cities press 1");
+        System.out.println("To exit program press #");
+        String input = scanner.nextLine();
+
+        while (true) {
+            if (input.equals(String.valueOf(1))) {
+                planner.displayListOfCities(cities);
+            } else if (input.equals("#")) {
+                System.out.println("Good bye!");
+                break;
+            }
+
+            System.out.println("Enter a city from which you would like to start your trip: ");
+            String startCity = scanner.nextLine();
+            route.add(startCity);
+            String current = startCity;
+            while (true) {
+                String nextCity = planner.getNextCity(current);
+                route.add(nextCity);
+                if (nextCity.equals(startCity)) break;
+                current = nextCity;
+            }
+            planner.printRoute(route);
+            break;
+        }
+
+
+    }
+
+    public void defineCity(String cityName) {
+        if (!cities.contains(cityName)) {
+            cities.add(cityName);
+            flights.put(cityName, new ArrayList<>());
+        }
+    }
+
+    private ArrayList<String> getDestinations(String fromCity) {
+        return flights.get(fromCity);
+    }
+
+    public void readFlightEntry() throws IOException {
         for (String line : list) {
-            String[] trip = line.split("->");
-            String from = trip[0].trim();
-            String to = trip[1].trim();
+            String[] cities = line.split("->");
+            String fromCity = cities[0].trim();
+            String toCity = cities[1].trim();
+            defineCity(fromCity);
+            defineCity(toCity);
+            getDestinations(fromCity).add(toCity);
+        }
+    }
 
-            if (!cities.contains(from)) {
-                cities.add(from);
-                flights.put(from, to);
+    public void displayListOfCities(ArrayList<String> cities) {
+        for (String city : cities) {
+            System.out.println(city);
+        }
+    }
 
+    public String getNextCity(String city) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> destinations = getDestinations(city);
+        String nextCity;
+        System.out.println("From " + city + " you can fly directly to:");
+        displayListOfCities(destinations);
+        while (true) {
+            System.out.println("Where do you want to go from " + city + "? ");
+            nextCity = scanner.nextLine();
+            if (!destinations.contains(nextCity)) {
+                System.out.println("Invalid city name or these cities are not connected by direct flight");
+            } else {
+                break;
+            }
+        }
+        System.out.println(nextCity);
+        return nextCity;
+    }
+
+    public void printRoute(ArrayList<String> route) {
+        System.out.println("The route you have chosen for this trip is: ");
+        for (int i = 0; i < route.size(); i++) {
+            if (i == route.size() - 1) {
+                System.out.print(route.get(i));
+            } else {
+                System.out.print(route.get(i) + " -> ");
             }
 
         }
-
-        System.out.println(cities);
-        System.out.println(flights);
     }
 
 }
